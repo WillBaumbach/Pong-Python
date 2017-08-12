845bbf1ee6fa12330e91a8dacdf25ff873f10e6f
williambaumbach1212 <will@dailygrinder.com>
2017-08-12 18:26:13 -0400

    Added class for scoring and working on game over screen.

--- Pong.py
+++ Pong.py
@@ -2,33 +2,40 @@ import pygame, sys
 from pygame.locals import *
 from Paddle import *
 from Ball import *
+from Score import *
 from random import randint
 
 #Start Pygame
 pygame.init()
+pygame.font.init()
+running = True
+limit = 5
 
 #Variables.
 SCREEN_WIDTH = 780
 SCREEN_HEIGHT = 720
 FRAMERATE = 60
-score = [0,0]
 BG = pygame.image.load("assets/pongBG.jpg")
+FONT = pygame.font.Font("assets/munro.ttf", 30)
 
 #Create screen, clock, and name window.
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('PONG')
 clock = pygame.time.Clock()
 
+# Create the scorer
+scorer = Scorer()
+
 # Create the Paddles(x,y,length,width,speed,playernumber).
 paddle2 = Paddle(20, 10, 100, 20, 10, 2)
 paddle1 = Paddle(740, 10, 100, 20, 10, 1)
 
-# Spawn the Ball(x, y, xspeed, yspeed, size).
+# Spawn the Ball(x, y, xspeed, yspeed, size) at a random speed and direction.
 randomx = randint(2,3)
 randomy = randint(3,5)
-if(randint(0,100) > 50):
+if(randint(0,1) == 1):
     randomx = -randomx
-if(randint(0,600) > 300):
+if(randint(0,1) == 1):
     randomy = -randomy
 pongball = Ball(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, randomx, randomy, 20)
 
@@ -40,70 +47,63 @@ def drawPaddle(x, y, length, width):
 def drawBall(x, y, size):
     pygame.draw.rect(DISPLAYSURF, (255,255,255), (x,y,size,size))
 
-# Used to create a random number for speed of ball.
-def randomNumber(a,b):
-    rand = randint(a,b)
-    if(randint(1,2) == 1):
-        rand = -rand
-    return rand
-
-# Determine if a point has been scored, and reset the ball location and speed.
-def checkPoint(x):
-    if(x < 0):
-        score[1] += 1
-        pongball.xpos = SCREEN_WIDTH/2
-        pongball.ypos = SCREEN_HEIGHT/2
-        pongball.xspeed = randomNumber(2,3)
-        pongball.yspeed = randomNumber(3,5)
-        pongball.pause = 2 * FRAMERATE
-    elif(x > 760):
-        score[0] += 1
-        pongball.xpos = SCREEN_WIDTH/2
-        pongball.ypos = SCREEN_HEIGHT/2
-        pongball.xspeed = randomNumber(2,3)
-        pongball.yspeed = randomNumber(3,5)
-        pongball.pause = 2 * FRAMERATE
-
-def checkWin(s):
-    if(s[0] == 5 or s[1] == 5):
-        handleWin()
-
-def handleWin(pause):
-    while pause:
-        # Print box for "Quit" and "Play again"
-        end = True
-        if end == True:
-            pygame.quit()
-            sys.exit()
-        
-
 # Game Loop
 while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
-    #Checking Variables
-    k = pygame.key.get_pressed()
-    paddle1.move(paddle1.ypos, k, paddle1.number)
-    paddle2.move(paddle2.ypos, k, paddle2.number)
-    pongball.move(pongball.xpos, pongball.ypos, pongball.xspeed, pongball.yspeed, paddle2, paddle1)
-    
-    # Drawing screen
-    DISPLAYSURF.blit(BG, (0,0))
-    
-    
-    # Draw ball and paddles
-    drawBall(pongball.xpos, pongball.ypos, pongball.size)
-    drawPaddle(paddle1.xpos, paddle1.ypos, paddle1.width, paddle1.length)
-    drawPaddle(paddle2.xpos, paddle2.ypos, paddle2.width, paddle2.length)
+        #Checking Variables
+        k = pygame.key.get_pressed()
+        paddle1.move(paddle1.ypos, k, paddle1.number)
+        paddle2.move(paddle2.ypos, k, paddle2.number)
+        pongball.move(pongball.xpos, pongball.ypos, pongball.xspeed, pongball.yspeed, paddle2, paddle1)
+        
+        # Drawing screen
+        DISPLAYSURF.blit(BG, (0,0))
+        
+        # Draw ball and paddles
+        drawBall(pongball.xpos, pongball.ypos, pongball.size)
+        drawPaddle(paddle1.xpos, paddle1.ypos, paddle1.width, paddle1.length)
+        drawPaddle(paddle2.xpos, paddle2.ypos, paddle2.width, paddle2.length)
 
-    # Check for a point
-    checkPoint(pongball.xpos)
-    checkWin(score)
+        # Check for a point
+        scorer.checkPoint(pongball.xpos, pongball, SCREEN_WIDTH, SCREEN_HEIGHT, FRAMERATE)
+        scorer.printScore(scorer.player1score, scorer.player2score)
+        running = scorer.checkWin(scorer.player1score, scorer.player2score, limit)
 
-    # Update display and tick clock.
-    pygame.display.update()
-    pygame.display.flip()
-    clock.tick(FRAMERATE)
+        # Update display and tick clock.
+        pygame.display.update()
+        pygame.display.flip()
+        clock.tick(FRAMERATE)
+
+    # WIP: Trying to implement a game over screen with "Play again" and "Quit"
+    while not running:
+        
+        # Print box for "Play Again"
+        # pygame.draw.rect(DISPLAYSURF, (0,204,0), (100,375,200,150))
+        # Print box for "Exit"
+        # pygame.draw.rect(DISPLAYSURF, (204,0,0), (480,375,200,150))
+        if(scorer.player1score == limit):
+            string = "Player 1 wins!"
+            winText = FONT.render(string, True, (255,255,255))
+            DISPLAYSURF.blit(winText, (100,200))
+            pygame.display.update()
+            
+        else:
+            string = "Player 2 wins!"
+            winText = FONT.render(string, True, (255,255,255))
+            DISPLAYSURF.blit(winText, (500,200))
+            pygame.display.update()
+        
+
+        # Update display and tick clock.
+        
+        pygame.display.flip()
+        clock.tick(FRAMERATE)
+
+
+
+        
